f043c77b0503a872808c18b1e2977d42c40f4de5
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-10-11 11:50:44 +1000

    изменения 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -106,8 +106,7 @@ namespace ISRPO_Cherednichenko_PR7
         private void butt1_Click(object sender, RoutedEventArgs e)
         {
             a = a + 1;
-            zakaz.Text = zakaz.Text += a +" "+ PB1.Text +" "+ PB1c.Text + Environment.NewLine;
-
+            zakaz.Text = zakaz.Text += a + " " + PB1.Text + " " + PB1c.Text + Environment.NewLine;
             d = d + 180;
             itog.Text = d.ToString();
 
@@ -117,6 +116,112 @@ namespace ISRPO_Cherednichenko_PR7
         {
             a = a + 1;
             zakaz.Text = zakaz.Text += a +" "+ PB2.Text +" "+ PB2c.Text + Environment.NewLine;
+            d = d + 180;
+            itog.Text = d.ToString();
+        }
+
+        private void butt3_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + PB3.Text + " " + PB3c.Text + Environment.NewLine;
+            d = d + 340;
+            itog.Text = d.ToString();
+        }
+
+        private void butt4_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + VB1.Text + " " + VB1c.Text + Environment.NewLine;
+            d = d + 410;
+            itog.Text = d.ToString();
+        }
+
+        private void butt5_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + VB2.Text + " " + VB2c.Text + Environment.NewLine;
+            d = d + 290;
+            itog.Text = d.ToString();
+        }
+
+        private void butt6_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + VB3.Text + " " + VB3c.Text + Environment.NewLine;
+            d = d + 115;
+            itog.Text = d.ToString();
+        }
+
+        private void butt7_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + N1.Text + " " + N1c.Text + Environment.NewLine;
+            d = d + 40;
+            itog.Text = d.ToString();
+        }
+
+        private void butt8_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + N2.Text + " " + N2c.Text + Environment.NewLine;
+            d = d + 120;
+            itog.Text = d.ToString();
+        }
+
+        private void butt9_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + N3.Text + " " + N3c.Text + Environment.NewLine;
+            d = d + 200;
+            itog.Text = d.ToString();
+        }
+
+        private void butt10_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + D1.Text + " " + D1c.Text + Environment.NewLine;
+            d = d + 280;
+            itog.Text = d.ToString();
+        }
+
+        private void butt11_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + D2.Text + " " + D2c.Text + Environment.NewLine;
+            d = d + 200;
+            itog.Text = d.ToString();
+        }
+
+        private void butt12_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + D3.Text + " " + D3c.Text + Environment.NewLine;
+            d = d + 100;
+            itog.Text = d.ToString();
+        }
+
+        private void butt13_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + Z1.Text + " " + Z1c.Text + Environment.NewLine;
+            d = d + 215;
+            itog.Text = d.ToString();
+        }
+
+        private void butt14_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + Z2.Text + " " + Z2c.Text + Environment.NewLine;
+            d = d + 150;
+            itog.Text = d.ToString();
+        }
+
+        private void butt15_Click(object sender, RoutedEventArgs e)
+        {
+            a = a + 1;
+            zakaz.Text = zakaz.Text += a + " " + Z3.Text + " " + Z3c.Text + Environment.NewLine;
+            d = d + 550;
+            itog.Text = d.ToString();
         }
     }
 }
